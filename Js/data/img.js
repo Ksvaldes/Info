ee377242ed85a3418f8d91c73ff9e39d60f01762
Ksvaldes <ksvaldes@sscl.edu.gt>
2022-09-06 16:54:16 -0600

    Es un ejemplo

---
+++ Js/data/img.js
@@ -0,0 +1,22 @@
+export let producto = [
+    {
+        img : 'https://us.123rf.com/450wm/arcady31/arcady311605/arcady31160500120/57592165-icono-de-la-manzana-roja.jpg',
+        descripcion: 'Manzanas de Guatemala',
+        precio: 100
+    },
+    {
+        img : 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/naranja-portada-1657997149.jpg',
+        descripcion: 'Naranja de Guatemala',
+        precio: 50
+    },
+    {
+        img : 'https://c8.alamy.com/zoomses/9/8943248bf0764b298b579dddfa46be79/2a4w7wr.jpg',
+        descripcion: 'Pera de Guatemala',
+        precio: 75
+    },
+    {
+        img : 'https://cdn.elnacional.com/wp-content/uploads/2018/01/durazno-tiene-multiples-propiedades-medicinales_221238.jpg',
+        descripcion: 'Durazno de Guatemala',
+        precio: 30
+    }
+];
---
+++ Js/main.js
@@ -0,0 +1,21 @@
+import {item} from "./componentes/itemGaleria.js";
+import {producto} from "./data/img.js"
+let root = document.querySelector("#root");
+
+
+function cargarGaleria(){
+
+    let galeria = document.querySelector("#galeria");
+
+    producto.forEach(function(elemento,indice){
+
+        let div = document.createElement("div");
+        div.setAttribute("class","item");
+        div.innerHTML = item(indice);
+
+        galeria.appendChild(div);
+    })
+
+}
+
+cargarGaleria();
